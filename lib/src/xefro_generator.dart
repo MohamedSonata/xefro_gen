a262e4da856afb051d1a81120fd2979d2ce1a178
MohamedSonata <sonata.1993@yahoo.com>
2024-07-06 08:03:38 +0300

    Added An Condition to check if any field not has CommandFlag Annotation not added it to GenerateConde Function

--- lib/src/xefro_generator.dart
+++ lib/src/xefro_generator.dart
@@ -1,4 +1,6 @@
+// ignore: depend_on_referenced_packages
 import 'package:analyzer/dart/element/element.dart';
+// ignore: depend_on_referenced_packages
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:xefro_annotations/xefro_annotations.dart';
@@ -36,65 +38,60 @@ class XefroGenerator extends GeneratorForAnnotation<XefroGen> {
   ) {
     final buffer = StringBuffer();
     final coreChecker = TypeChecker.fromRuntime(CommandLineFlag);
- final className = visitor.className;
+    final className = visitor.className;
 
-  buffer.writeln('// Command Flags Method');
+    buffer.writeln('// Command Flags Method');
 //  buffer.writeln('extension ScrcpyCommandModelExtensions on $className {');
     buffer.writeln('  String _\$${className}toCommand($className instance) {');
     buffer.writeln('    List<String> options = [];');
 
     // Iterate over the fields of the class
     for (var field in element.fields) {
-      String? flagValue;
-      if (
-        coreChecker.firstAnnotationOfExact(field)?.getField('defaultValue')
-        ?.toBoolValue() ==true ||false) {
-        flagValue = "${coreChecker.firstAnnotationOfExact(field)!.getField('defaultValue')!.toBoolValue()!}";
-      } else {
-        flagValue = coreChecker.firstAnnotationOfExact(field)?.getField('defaultValue')?.toStringValue();
+      final flag = coreChecker
+              .firstAnnotationOfExact(field)
+              ?.getField('flag')
+              ?.toStringValue() ??
+          '';
+      if (flag.isEmpty) {
+        continue;
       }
- 
-      final flag = coreChecker.firstAnnotationOfExact(field)?.getField('flag')?.toStringValue() ?? '';
-  
+      String? flagValue;
+
+      if (coreChecker.firstAnnotationOfExact(field)?.getField('defaultValue')?.toBoolValue() ==true ||false) {
+        flagValue ="${coreChecker.firstAnnotationOfExact(field)!.getField('defaultValue')!.toBoolValue()!}";
+      } else {
+        flagValue = coreChecker
+            .firstAnnotationOfExact(field)
+            ?.getField('defaultValue')
+            ?.toStringValue();
+      }
+
       // Generate command options based on variables
-      if(field.type.toString() == "bool?"){
-          final fieldType = field.type;  // Get
-       print("Field Type: ${field.type.toString()}");
-print('Field declared type: ${fieldType.getDisplayString(withNullability: true)}');
-         
-            buffer.writeln('    if (instance.${field.name}  == true) {');
-          
-
-      buffer.writeln('          options.add("$flag");');
-      buffer.writeln('    }');
-         
-          
-        }else{
-
-          if(flag.contains("=")){
-              buffer.writeln('    if (instance.${field.name} != null) {');
-
-      buffer.writeln('          options.add("$flag${flagValue ?? "\${instance.${field.name}}" }");');
-      buffer.writeln('    }');
-
-          }else{
-              buffer.writeln('    if (instance.${field.name} != null) {');
-
-      buffer.writeln('          options.add("$flag${flagValue ?? " \${instance.${field.name}}" }");');
-      buffer.writeln('    }');
+      if (field.type.toString() == "bool?") {
+        buffer.writeln('    if (instance.${field.name}  == true) {');
+        buffer.writeln('          options.add("$flag");');
+        buffer.writeln('    }');
+      } else {
+        if (flag.contains("=")) {
+          buffer.writeln('    if (instance.${field.name} != null) {');
+          buffer.writeln(
+              '          options.add("$flag${flagValue ?? "\${instance.${field.name}}"}");');
+          buffer.writeln('    }');
+        } else {
+          buffer.writeln('    if (instance.${field.name} != null) {');
+          buffer.writeln(
+              '          options.add("$flag${flagValue ?? " \${instance.${field.name}}"}");');
+          buffer.writeln('    }');
         }
-          }
+      }
 
-          // print("FieldName : ${field.name}");
-          //  print("FieldRunTimeStype:${coreChecker.firstAnnotationOfExact(field)?.runtimeType} ");
-
-    
-   
+      // print("FieldName : ${field.name}");
+      //  print("FieldRunTimeStype:${coreChecker.firstAnnotationOfExact(field)?.runtimeType} ");
     }
-      buffer.writeln(" return 'scrcpy \${options.join(' ')}';");
-  buffer.writeln('}');
-  // buffer.writeln('}');
-  buffer.writeln("//sssssssssssssss");
+    buffer.writeln(" return 'scrcpy \${options.join(' ')}';");
+    buffer.writeln('}');
+    // buffer.writeln('}');
+    buffer.writeln("//sssssssssssssss");
 
     return buffer.toString();
   }
